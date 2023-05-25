7a3497b243f99d71599d5f2bee6eecd1c91e7977
Aninditaroy <anindita.roy.mu@gmail.com>
2023-05-25 20:55:48 +0600

    vercel link added

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
   try {
-    await client.connect();
+    client.connect();
     const perfumeCollection = client.db("bottledHeaven").collection("perfumes");
 
     // Auth for JWT token
